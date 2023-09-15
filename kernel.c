869ae1ca4708bc014c8baf5d41218eaed7732331
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-15 21:26:03 +0200

    Now cmain prints on the screen

--- kernel.c
+++ kernel.c
@@ -1,4 +1,39 @@
+#define BYTES_PER_PIXEL 2
+#define LINES 25
+#define COLUMNS 80
+#define LIGHT_GREY 0x07
+#define VIDEO_MEMORY_START 0xb8000
+#define BLANCK_CHAR ' '
 
 void cmain(void) {
-    
+
+    const char *str = "my first kernel";
+	char *vidptr = (char*)VIDEO_MEMORY_START;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+    /* clear the screen */
+    while(j < COLUMNS * LINES * BYTES_PER_PIXEL) {
+		/* blank character */
+		vidptr[j] = BLANCK_CHAR;
+
+		/* attribute-byte - light grey on black screen */
+		vidptr[j+1] = LIGHT_GREY; 		
+		j += BYTES_PER_PIXEL;
+	}
+
+	j = 0;
+
+    /* this loop writes the string to video memory */
+	while(str[j] != '\0') {
+		/* the actual character in ascii */
+		vidptr[i] = str[j];
+
+		/* the color of the previos char */
+		vidptr[i+1] = LIGHT_GREY;
+		j++;
+		i += BYTES_PER_PIXEL;
+	}
+
+	return;
 }

1af2f4c065c96bedd97886d5bcd59bbc5bba1dd8
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-15 21:11:44 +0200

    defined the name start_stack_space

--- kernel.asm
+++ kernel.asm
@@ -5,7 +5,11 @@ global start
 extern cmain    ;cmain is defined in a c file
 
 start:
-  cli 			;block interrupts
-  mov esp, start_stack_space	
+  cli 			                ;block interrupts
+  mov esp, start_stack_space	;set stack pointer
   call cmain
-  hlt		 	;halt the CPU
+  hlt		 	                ;halt the CPU
+
+section .bss
+resb 8192		;8KB for stack
+start_stack_space:

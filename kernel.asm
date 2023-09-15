4c48ed6880f1c1fd36104c0f1c02c57c95cb768e
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-15 21:10:53 +0200

    added entrypoint

--- kernel.asm
+++ kernel.asm
@@ -2,4 +2,10 @@ bits 32			;nasm directive - 32 bit
 section .text
 
 global start
-extern kmain
+extern cmain    ;cmain is defined in a c file
+
+start:
+  cli 			;block interrupts
+  mov esp, start_stack_space	
+  call cmain
+  hlt		 	;halt the CPU

a0208e21c30d21e398fafd1d83201e51ef5739ed
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-16 21:18:51 +0200

    added definition of keaboard handler

--- kernel.asm
+++ kernel.asm
@@ -9,9 +9,11 @@ section .text
     dd - (0x1BADB002 + 0x00) ; checksum field m+f+c should be zero
 
 global start
-extern cmain    ;cmain is defined in a c file
 global load_idt
+global keyboard_handler
 
+extern cmain    ;cmain is defined in a c file
+extern keyboard_handler_main
 
 load_idt:
 	mov edx, [esp + 4]
@@ -19,6 +21,10 @@ load_idt:
 	sti
 	ret
 
+
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
 
 
 start:

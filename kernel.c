ff5345b3cdfd74f30aa8e552d8e6b5bad83e6377
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-16 21:19:48 +0200

    implemented keyboard_handler_main

--- kernel.c
+++ kernel.c
@@ -103,6 +103,25 @@ void kb_init(void)
 }
 
 
+void keyboard_handler_main(void) {
+	unsigned char status;
+	char keycode;
+
+	/* write EOI */
+	write_port(0x20, 0x20);
+
+	status = read_port(KEYBOARD_STATUS_PORT);
+	/* Lowest bit of status will be set if buffer is not empty */
+	if (status & 0x01) {
+		keycode = read_port(KEYBOARD_DATA_PORT);
+		if(keycode < 0)
+			return;
+		vidptr[current_cursor_location++] = keyboard_map[keycode];
+		vidptr[current_cursor_location++] = 0x07;
+	}
+}
+
+
 void cmain(void) {
 
     const char *str = "Hello world from my first kernel";

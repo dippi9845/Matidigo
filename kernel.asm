bbd6c4ea85ef363d09d8015ffb10d2e226b58567
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-16 21:23:45 +0200

    defined read_port and write port

--- kernel.asm
+++ kernel.asm
@@ -11,9 +11,23 @@ section .text
 global start
 global load_idt
 global keyboard_handler
+global read_port
+global write_port
 
 extern cmain    ;cmain is defined in a c file
 extern keyboard_handler_main
+
+read_port:
+	mov edx, [esp + 4]
+			;al is the lower 8 bits of eax
+	in al, dx	;dx is the lower 16 bits of edx
+	ret
+
+write_port:
+	mov   edx, [esp + 4]    
+	mov   al, [esp + 4 + 4]  
+	out   dx, al  
+	ret
 
 load_idt:
 	mov edx, [esp + 4]

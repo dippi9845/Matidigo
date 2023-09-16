002579d9a45e54bb7581daf16121982968734cd1
Filippo Di Pietro <filippodp9845@gmail.com>
2023-09-16 21:12:35 +0200

    added Interrupt Descriptor Table definition

--- kernel.c
+++ kernel.c
@@ -31,6 +31,16 @@ char *vidptr = (char*)VIDEO_MEMORY_START;
 
 
 
+struct IDT_entry {
+	unsigned short int offset_lowerbits;
+	unsigned short int selector;
+	unsigned char zero;
+	unsigned char type_attr;
+	unsigned short int offset_higherbits;
+};
+
+struct IDT_entry IDT[IDT_SIZE];
+
 
 void cmain(void) {
 

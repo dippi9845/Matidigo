d0b79fb089292fb44994c2d8a3b1ec745f186652
Filippo Di Pietro <filippodp9845@gmail.com>
2023-10-14 00:28:33 +0200

    removed keyboard support from kernel asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -9,45 +9,23 @@ section .text
     dd - (0x1BADB002 + 0x03) ; checksum field m+f+c should be zero
 
 global start
-global load_idt
-global keyboard_handler
-global read_port
-global write_port
 
 extern CMain    ; is defined in a cpp file
-extern keyboard_handler_main
 extern CallConstructors 
 
-read_port:
-	mov edx, [esp + 4]
-			;al is the lower 8 bits of eax
-	in al, dx	;dx is the lower 16 bits of edx
-	ret
-
-write_port:
-	mov   edx, [esp + 4]    
-	mov   al, [esp + 4 + 4]  
-	out   dx, al  
-	ret
-
-load_idt:
-	mov edx, [esp + 4]
-	lidt [edx]
-	sti
-	ret
-
-
-keyboard_handler:                 
-	call    keyboard_handler_main
-	iretd
-
-
 start:
-  cli 			                ;block interrupts
-  mov esp, start_stack_space	;set stack pointer
-  call CallConstructors
-  call CMain
-  hlt		 	                ;halt the CPU
+	cli 			                ;block interrupts
+	mov esp, start_stack_space	;set stack pointer
+	call CallConstructors
+	push eax
+	push ebx
+	call CMain
+			 	                
+
+stop:
+	cli
+	hlt ;halt the CPU
+	jmp stop
 
 section .bss
 resb 8192		;8KB for stack

1d55f7c77983599f3ebad9ac0a82321978bfad4f
Filippo Di Pietro <filippodp9845@gmail.com>
2023-10-14 01:22:26 +0200

    Update global_descriptor_table.cpp

--- src/global_descriptor_table.cpp
+++ src/global_descriptor_table.cpp
@@ -51,9 +51,27 @@ SegmentDescriptor::SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type
 }
 
 uint32_t SegmentDescriptor::Base() {
+    uint8_t* target = (uint8_t*)this;
+
+    uint32_t result = target[7];
+    result = (result << 8) + target[4];
+    result = (result << 8) + target[3];
+    result = (result << 8) + target[2];
+
+    return result;
 }
 
 uint32_t SegmentDescriptor::Limit() {
+    uint8_t* target = (uint8_t*)this;
+
+    uint32_t result = target[6] & 0xF;
+    result = (result << 8) + target[1];
+    result = (result << 8) + target[0];
+
+    if((target[6] & 0xC0) == 0xC0)
+        result = (result << 12) | 0xFFF;
+
+    return result;
 }
 
 matidigo::GlobalDescriptorTable::GlobalDescriptorTable() {
